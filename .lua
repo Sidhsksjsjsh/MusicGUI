acd56cfaf6f602207d9c75934e5cca887a36f3d6
Fhri <117529823+Sidhsksjsjsh@users.noreply.github.com>
2023-11-18 10:17:46 +0900

    Update .lua

--- .lua
+++ .lua
@@ -1,7 +1,12 @@
+if player.PlayerGui:FindFirstChild("CLOCK") then
+    player.PlayerGui:FindFirstChild("CLOCK"):Destroy()
+end
+
 local player = game.Players.LocalPlayer
-local gui = player:WaitForChild("PlayerGui")
+local gui = player.PlayerGui
 local screenGui = Instance.new("ScreenGui")
 screenGui.Parent = gui
+screenGui.Name = "CLOCK"
 
 local frame = Instance.new("Frame")
 frame.Size = UDim2.new(0, 200, 0, 20)
@@ -11,26 +16,29 @@ frame.Parent = screenGui
 
 local timeLabel = Instance.new("TextLabel")
 timeLabel.Size = UDim2.new(1, 0, 1, 0)
-timeLabel.Text = "0:00 / 0:00"
+timeLabel.Text = "--:--:--"
 timeLabel.TextColor3 = Color3.new(1, 1, 1)
 timeLabel.Parent = frame
+timeLabel.BackgroundTransparency = 1
 
 local musicIdTextBox = Instance.new("TextBox")
 musicIdTextBox.Size = UDim2.new(0, 150, 0, 20)
 musicIdTextBox.Position = UDim2.new(0, 25, 0, 40)
 musicIdTextBox.PlaceholderText = "Masukkan ID Musik"
 musicIdTextBox.Parent = screenGui
+musicIdTextBox.visible = false
 
 local playButton = Instance.new("TextButton")
 playButton.Size = UDim2.new(0, 50, 0, 20)
 playButton.Position = UDim2.new(0, 180, 0, 40)
 playButton.Text = "Putar"
 playButton.Parent = screenGui
+playButton.Visible = false
 
 local musicLength = 0
 local currentTime = 0
 
-playButton.MouseButton1Click:Connect(function()
+--[[playButton.MouseButton1Click:Connect(function()
     local success, result = pcall(function()
         musicLength = game:GetService("SoundService"):GetWaveUploadedDuration(tonumber(musicIdTextBox.Text))
     end)
@@ -49,14 +57,8 @@ function updateLabelText()
     local totalMinutes = math.floor(musicLength / 60)
     local totalSeconds = musicLength % 60
     timeLabel.Text = string.format("%d:%02d / %d:%02d", minutes, seconds, totalMinutes, totalSeconds)
-end
+end]]
 
-while wait(1) do
-    if currentTime < musicLength then
-        currentTime = currentTime + 1
-        updateLabelText()
-    else
-        warn("Musik selesai.")
-        break
-    end
+while wait() do
+    timeLabel.Text = os.date("%X")
 end
